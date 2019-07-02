a6a124b6ffd8cf0fd409a63aa7e5cf5379c40baa
Prashant Singh <prashantxc8@gmail.com>
2019-07-02 15:33:51 +0530

    fix: Url addressing

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const request = require('request');
 
-const url = 'https://api.darksky.net/forecast/2e64da1cc0ec196717545eb97658cf8d/37.8267,-122.4233';
+// Keys
+const WEATHER_URL = 'https://api.darksky.net/forecast/2e64da1cc0ec196717545eb97658cf8d/37.8267,-122.4233';
+const GEOCODE_URL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicHJhc2hhbnR4YyIsImEiOiJjanhsaGNuNzcwNjYwM3JtZzUwNzN6OWNoIn0.I-skCxjmKCtglI72PSHJcg&limit=1';
 
-request({ url: url, json: true }, (error, response) => {
+// Weather
+request({ url: WEATHER_URL, json: true }, (error, response) => {
     if(error) {
         console.log('Weather Service is not available right now');
     } else if(response.body.error) {
@@ -10,12 +13,10 @@ request({ url: url, json: true }, (error, response) => {
     } else {
         console.log(`${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} degree out. There is a ${response.body.currently.precipProbability}% chance of rain.`);
     }
-})
+});
 
 //Geocoding
-const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicHJhc2hhbnR4YyIsImEiOiJjanhsaGNuNzcwNjYwM3JtZzUwNzN6OWNoIn0.I-skCxjmKCtglI72PSHJcg&limit=1';
-
-request({ url: geocodeURL, json: true }, (error, response) => {
+request({ url: GEOCODE_URL, json: true }, (error, response) => {
     if(error) {
         console.log('Unable to connect with location service.');
     } else if(response.body.features.length === 0) {
@@ -26,4 +27,4 @@ request({ url: geocodeURL, json: true }, (error, response) => {
         console.log(latitude, longitude);
     }
     
-})
+});
