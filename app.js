df5f4664a1ea71a1f3874a5ab31469675c67fd40
Prashant Singh <prashantxc8@gmail.com>
2019-07-02 14:36:09 +0530

    Error Handling for response of DarkSky

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const request = require('request');
 const url = 'https://api.darksky.net/forecast/2e64da1cc0ec196717545eb97658cf8d/37.8267,-122.4233';
 
 request({ url: url, json: true }, (error, response) => {
-    console.log(`${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} degree out. There is a ${response.body.currently.precipProbability}% chance of rain.`);
+    if(error) {
+        console.log('Weather Service is not available right now');
+    } else if(response.body.error) {
+        console.log('Problem with location.');
+    } else {
+        console.log(`${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} degree out. There is a ${response.body.currently.precipProbability}% chance of rain.`);
+    }
 })
 
